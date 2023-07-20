cbd1af55f6993f2ff142901af758c972595bcb1a
Patel1001 <130114311+Patel1001@users.noreply.github.com>
2023-07-19 21:30:36 -0300

    Update Weatherscript.js

--- Weatherscript.js
+++ Weatherscript.js
@@ -69,19 +69,5 @@ let weather = {
     }
   });
 
-  src = "https://smtpjs.com/v3/smtp.js";
-
-  function sendEmail(){
-    Email.send({
-      Host : "smtp.elasticemail.com",
-      Username : "username",
-      Password : "password",
-      To : 'them@website.com',
-      From : "you@isp.com",
-      Subject : "This is the subject",
-      Body : "And this is the body"
-  }).then(
-    message => alert(message)
-  );
-  }
+  
   
